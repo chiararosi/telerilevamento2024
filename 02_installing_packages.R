77d33c84cb344c55808fec6da80c16ac6c25d542
chiararosi <162603141+chiararosi@users.noreply.github.com>
2024-03-07 17:50:45 +0100

    Create 02_installing_packages.R

---
+++ 02_installing_packages.R
@@ -0,0 +1,12 @@
+#Installing new packages in R
+#scarichiamo terra
+#la funzione per scaricare i pacchetti installa.packages() installa le funzioni da altre repositories di R o dai file locali del nostro computer
+install.packages("terra") 
+#l'argomento che vado ad inserire è il pacchetto che vado ad installare
+#dobbiamo usare le virgolette
+#per controllare che il pacchetto funziona senza errori lo richiamo in R tramite library()
+#library() mi carica il pacchetto dentro R
+#library("terra") vedere se il pacchetto è stato correttamente installato
+#per rimuovere dei pacchetti funzione remove.packages()
+#si possono anche aggiornare i pacchetti
+#per cercare le persone su githup si scrive su cerca user:nomedelprofilo(=ducciorocchini)
