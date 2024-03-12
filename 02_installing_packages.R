7e9f32c55cdd5c43024879d5ccba24506ecd1198
chiararosi <162603141+chiararosi@users.noreply.github.com>
2024-03-12 17:31:55 +0100

    Update 02_installing_packages.R

--- 02_installing_packages.R
+++ 02_installing_packages.R
@@ -10,3 +10,36 @@ install.packages("terra")
 #per rimuovere dei pacchetti funzione remove.packages()
 #si possono anche aggiornare i pacchetti
 #per cercare le persone su githup si scrive su cerca user:nomedelprofilo(=ducciorocchini)
+
+# Installing new packages in R
+
+# install.packages("terra")
+library(terra)
+
+library(devtools)
+
+# install the imageRy package from GitHub
+devtools::install_github("ducciorocchini/imageRy")
+
+library(imageRy)
+#con CRAN posso trovare i pacchetti controllati da sviluppatori del software R
+#posso anche installare pacchetti non controllati da R, come imageRy
+#lo installo tramite github
+#la funzione install.packages("") è quella di R
+#la funzione install_github("") è dipendente da devtools
+#devo prima installare devtools, che è sul CRAN
+#devtools contiene la funzione install_github, che pesca imageRy
+#se vogliamo far vedere da dove provengono le nostre funzioni: devtools::install_github("ducciorocchini/imageRy")
+#dico che è una funzione di questo pacchetto che si chiama devtools: funzione che deriva dal pacchetto che si chiama devtools
+#si può anche scrivere semplicemente install_gethub()
+install.packages("devtools") #pacchetto dedicato allo sviluppo
+#c'è sempre da mettere le virgolette quando voglio prendere qualcosa al di fuori di R (come scaricare un pacchetto dal CRAN)
+
+#tutte le volte che usiamo dei pacchetti li si scrivono tutti in cima insieme, con library() 
+#library()= require()
+#si può usare una funzione di devtools che è install_github("") va nell'account di qualcuno e vede se c'è il pacchetto con il suo nome
+#R è un software case sensitive
+#una volta installato il pacchetto si manda library()
+
+#imageRy fornisce funzioni e dati che si possono usare in maniera molto veloce
+#Control+S= commit changes, salva lo script
