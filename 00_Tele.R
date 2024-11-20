fa3142467735ce514aae9a8bb8750c76996f6837
chiararosi <162603141+chiararosi@users.noreply.github.com>
2024-11-20 17:27:08 +0100

    Update 00_Tele.R

--- 00_Tele.R
+++ 00_Tele.R
@@ -1,1 +1,5 @@
-
+#i commenti sono importantissimi, il software così non legge nulla
+#devo riempire l'esame di commenti
+#attraverso questo cancelletto posso scrivere quello che voglio su R studio
+# <- questo è il simbolo dell'assegnazione
+#ovviamente su R quando scrivo il nome del vettore e basta mi esce fuori gli oggetti che compongono il vettore come output
